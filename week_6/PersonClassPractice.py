5573b27125d48a41764129db9b6fbd98ebd5d4fc
teichopsia <anthomelanous@hotmail.com>
2015-10-15 15:54:37 -0500

    PersonClassPractice added along with MiTPerson class. Showing examples of inheritance

--- week_6/PersonClassPractice.py
+++ week_6/PersonClassPractice.py
@@ -34,3 +34,20 @@ class Person(object):
     def __str__(self):
         ''' return self's name '''
         return self.name
+        
+# MitPerson will inherit from the PersonClass
+class MitPerson(Person):
+    nextIdNum = 0
+    
+    def __init__(self, name):
+        Person.__init__(self, name) #Initialize Person attributes
+        # new MITPerson attribute: a unique ID number
+        self.idNum = MitPerson.nextIdNum
+        MitPerson.nextIdNum += 1
+        
+    def getIdNum(self):
+        return self.idNum
+        
+    # sorting Mit people using their ID numbers, not name.
+    def __lt__(self, other):
+        return self.idNum < other.idNum
